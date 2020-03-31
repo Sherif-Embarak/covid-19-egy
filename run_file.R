9747aeaaaadbcfc6bd8338dbb9516bef6674bdcb
sherifembarak <sherif.embarak@gmail.com>
2020-03-31 20:09:54 +0200

    add date 31-03

--- run_file.R
+++ run_file.R
@@ -1,7 +1,14 @@
 rm(list=ls(all=TRUE))
 setwd("D:/work/git corona/")
+cat("\n ------------------- \n" , format(Sys.time()),"\n")
 library(lubridate)
-cond <- readline(prompt="Please press 1 for new row and press 2 for Run Scraper: ")
+args = commandArgs(trailingOnly=TRUE)
+cond <- 0 
+if(length(args)==0){
+  cond <- readline(prompt="Please press 1 for new row and press 2 for Run Scraper: ")
+}else{
+  cond <- as.numeric(args[1])
+}
 if(cond == 1){
   new_cases <- as.numeric(readline(prompt="Enter new cases: "))
   new_deaths <- as.numeric(readline(prompt="Enter new deaths: "))
@@ -18,8 +25,8 @@ if(cond == 1){
   rw$New.Deaths...60 <- new_deaths
   df <- rbind(df , rw)
   write.csv(df,"eg_covid.csv",row.names = F)
+  cat("New Recored add\n")
 }else if( cond == 2){
-  cat("Scraping...2")
   source('Scraper.R')
   values <- get_values()
   df <- read.csv("eg_covid.csv")
@@ -28,7 +35,8 @@ if(cond == 1){
   new_deaths <- values[2]
   total_recovered <- values[3]
   if(new_cases == rw$New.Cases & new_deaths==rw$New.Deaths & total_recovered == rw$Total.Recovered){
-    stop("Nothing new")
+    cat("Nothing new\n")
+    stop("Nothing new\n")
   }else{
     rw$Day <- format(Sys.Date(),"%m/%d/%Y")
     rw$New.Cases <- new_cases
@@ -40,7 +48,9 @@ if(cond == 1){
     rw$New.Deaths...60 <- new_deaths
     df <- rbind(df , rw)
     write.csv(df,"eg_covid.csv",row.names = F)
+    cat("Automaticlly New Recored add\n")
   }
 }
+cat("Running rmarkdown file ...\n")
 rmarkdown::render(input = "Corona.Rmd", output_file = "index.html")
 
