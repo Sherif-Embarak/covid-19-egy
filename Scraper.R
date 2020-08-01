ff94b1a6134f3e5470e28fe0610a965b4b255489
sherifembarak <sherif.embarak@gmail.com>
2020-08-02 00:28:41 +0200

    Update day 2/8

--- Scraper.R
+++ Scraper.R
@@ -5,7 +5,7 @@ get_values <- function(){
   page <- read_html("https://www.care.gov.eg/EgyptCare/Index.aspx")
   values <- page%>% html_nodes("[class='progress-value']")%>%html_text()
   values <- as.numeric(gsub("([0-9]+).*$", "\\1", values))
-  return(as.numeric(c(values[1],values[2] , values[6])))
+  return(as.numeric(c(values[1],values[2] , values[5])))
 }
 
 page <- read_html("D:/National Bank of Egypt - ATM and branches.html")
